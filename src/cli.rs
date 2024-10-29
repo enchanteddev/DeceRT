09836e230731e33cd9b3cba2aa1c467c9324f88e
enchanteddev <119434133+enchanteddev@users.noreply.github.com>
2024-10-29 22:38:48 +0530

    create-project + add_obc done

--- src/cli.rs
+++ src/cli.rs
@@ -5,13 +5,13 @@ pub fn add_obc(id: u32) -> io::Result<()> {
     create_dir(format!("obc{id}/entry"))?;
     create_dir(format!("obc{id}/lib"))?;
     File::create(format!("obc{id}/ports.hpp"))?;
-    File::create(format!("tasks.conf"))?;
+    File::create(format!("obc{id}/tasks.conf"))?;
     Ok(())
 }
 
 
-pub fn create_project(project_name: String) -> std::io::Result<()> {
-    let path_dir = Path::new(project_name.as_str());
+pub fn create_project(project_name: &str) -> std::io::Result<()> {
+    let path_dir = Path::new(project_name);
     fs::create_dir(path_dir)?;
     File::create(path_dir.join("sensors.json"))?;
     Ok(())
--- src/main.rs
+++ src/main.rs
@@ -9,10 +9,20 @@ fn main() {
         exit(0);
     }
     match args[1].as_str() {
+        "create-project" => {
+            let Some(project_name) = args.get(2) else {
+                println!("Usage: decert create-project <name>");
+                exit(1)
+            };
+            match cli::create_project(project_name) {
+                Ok(_) => println!("Project created"),
+                Err(e) => println!("Error: {}", e),
+            };
+        }
         "add-obc" => {
             let Ok(id) = args[2].parse::<u32>() else {
                 println!("Usage: decert add-obc <id>");
-                exit(0);
+                exit(2);
             };
             match cli::add_obc(id) {
                 Ok(_) => println!("New OBC created"),
