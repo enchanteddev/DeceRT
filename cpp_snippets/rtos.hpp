655bcf7e5b87ec83a819ba2c8312a6f64304a5bd
enchanteddev <119434133+enchanteddev@users.noreply.github.com>
2024-11-29 21:29:51 +0530

    changed r_read/write to port and sensor variants

--- cpp_snippets/rtos.hpp
+++ cpp_snippets/rtos.hpp
@@ -2,6 +2,10 @@ void runTask(void (*f)(void**), void* , int);
 
 void delay(int);
 
-void r_read(int, char*, int);
+void port_read(int, char*, int);
 
-void r_write(int, char*, int);
+void port_write(int, char*, int);
+
+void sensor_read(int, char*, int);
+
+void sensor_write(int, char*, int);
